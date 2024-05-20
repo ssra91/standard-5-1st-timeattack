8622cac4ba0cdcf33f8a3e800969ab874c90dfb9
sorakim <sora6418@gmail.com>
2024-05-20 15:48:49 +0900

    feat : 계산기 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
+
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input /> 만큼을
+        <button onClick={() => setCount(count + 1)}>더할게요</button>
+        <button onClick={() => setCount(count - 1)}>뺄게요</button>
         <button>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{count}</p>
       </div>
     </div>
   );
